bba1d6c11b53017ae85b3c55fb85c72d10af6732
Bas Zalmstra <bzalmstra@smart-robotics.nl>
2020-07-01 08:30:57 +0200

    feat: zoxide, git-fuzzy

--- setup.bash
+++ setup.bash
@@ -15,37 +15,3 @@ declare -a TRUELINE_SEGMENTS=(
 )
 
 source "$cur_dir/external/trueline/trueline.sh"
-
-export PATH=$PATH:~/.dotfiles/bin
-
-# zoxide
-eval "$(zoxide init bash)"
-
-_zoxide_completion() {
-    local cur prev words cword
-    _init_completion || return
-    COMPREPLY=()
-    case $prev in
-    -takes_file)
-        _filedir
-        return
-        ;;
-    esac
-    case ${cur} in
-    -*)
-        COMPREPLY=($(compgen -W '
-        -V --version
-        -h --help
-        -i --interactive
-        ' -- "${cur}"; ))
-        [[ COMPREPLY == *= ]] || compopt +o nospace
-        ;;
-    *)
-        # set -x
-        # COMPREPLY=($(compgen -W "$( zq  "${cur}" | awk '{ print $NF}' )" -- "${cur}") )
-        COMPREPLY=($(compgen -W "$( zq  "${cur}" | awk '{ print $NF}' )
-        "  ) )
-        ;;
-
-    esac
-} && complete -F _zoxide_completion -o nospace z
